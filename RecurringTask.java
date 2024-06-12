c122b2c38cd79ef481606d058ca6c0bf1bc314a3
Jayden <Jcoleman91@gatech.edu>
2024-06-12 11:28:34 -0400

    Task class implemented and added child class RecurringTask

---
+++ RecurringTask.java
@@ -0,0 +1,8 @@
+public class RecurringTask extends Task{
+    private Task[] tasks;
+
+    public Task exisitingTask(Task task) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProject'");
+    }
+}
---
+++ Task.java
@@ -0,0 +1,50 @@
+public class Task implements Assignable {
+
+    @Override
+    public void setProject() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProject'");
+    }
+
+    @Override
+    public void getProject() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProject'");
+    }
+
+    public void setTitle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProject'");
+    }
+
+    public String getTitle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProject'");
+    }
+
+    public void setStatus() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProject'");
+    }
+
+    public boolean getStatus() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProject'");
+    }
+
+    public void setDueDate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProject'");
+    }
+
+    public String getDueDate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProject'");
+    }
+
+    //return highest priority tsk based on status and priority number
+    public Task prioity() {
+        //TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProject'");
+    }
+}
